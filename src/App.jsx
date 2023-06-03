312554a01fbfa2019d46ebbf84d7ebc41b58bd4d
Brijrajsinh parmar <73275164+brijrajparmar27@users.noreply.github.com>
2023-06-03 23:27:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
+    
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
