c7b5f47d21d92eb1bcf68a7c281e689a2ef31686
Brijrajsinh Parmar <brijrajparmar27@gmail.com>
2023-11-12 23:59:31 +0530

    touch ups

--- src/Pages/Experience/Experience.jsx
+++ src/Pages/Experience/Experience.jsx
@@ -54,6 +54,54 @@ export const Experience = () => {
     }
   }, [inView]);
 
+  const variants = {
+    enter: () => {
+      return {
+        x: -200,
+        opacity: 0,
+      };
+    },
+    center: {
+      zIndex: 1,
+      x: 0,
+      opacity: 1,
+    },
+    exit: () => {
+      return {
+        zIndex: 0,
+        x: -200,
+        opacity: 0,
+      };
+    },
+  };
+
+  const Rvariants = {
+    enter: () => {
+      return {
+        x: 200,
+        opacity: 0,
+      };
+    },
+    center: {
+      zIndex: 1,
+      x: 0,
+      opacity: 1,
+    },
+    exit: () => {
+      return {
+        zIndex: 0,
+        x: 200,
+        opacity: 0,
+      };
+    },
+  };
+
+  const [[page, direction], setPage] = useState([0, 0]);
+
+  const paginate = (newDirection) => {
+    setPage([page + newDirection, newDirection]);
+  };
+
   return (
     <div
       className="page text-subtext bg-bg_gray text-white flex justify-center items-center"
@@ -79,6 +127,7 @@ export const Experience = () => {
                   onClick={() => {
                     console.log("prev");
                     id > 1 && setId((prev) => prev - 1);
+                    id > 1 && paginate(-1);
                   }}
                 />
               </motion.div>
@@ -95,21 +144,51 @@ export const Experience = () => {
                   onClick={() => {
                     console.log("next");
                     id < experience.length && setId((prev) => prev + 1);
+                    id < experience.length && paginate(1);
                   }}
                 />
               </motion.div>
             </div>
             <div className="flex w-full justify-center items-center gap-10">
-              <AnimatePresence initial={false}>
+              <AnimatePresence
+                initial={false}
+                custom={direction}
+                mode="popLayout"
+              >
                 <motion.img
+                  key={experience[id - 1].id}
                   src={experience[id - 1].logo}
                   alt=""
                   className="md:h-[125px] h-[70px]"
-                  animate={animateFromLeft}
+                  // animate={animateFromLeft}
+                  custom={direction}
+                  variants={variants}
+                  initial="enter"
+                  animate="center"
+                  exit="exit"
+                  transition={{
+                    x: { type: "spring", stiffness: 300, damping: 30 },
+                    opacity: { duration: 0.2 },
+                  }}
                 />
               </AnimatePresence>
-              <AnimatePresence initial={false}>
-                <motion.div>
+              <AnimatePresence
+                initial={false}
+                custom={direction}
+                mode="popLayout"
+              >
+                <motion.div
+                  custom={direction}
+                  variants={Rvariants}
+                  initial="enter"
+                  animate="center"
+                  exit="exit"
+                  transition={{
+                    x: { type: "spring", stiffness: 300, damping: 30 },
+                    opacity: { duration: 0.2 },
+                  }}
+                  key={experience[id - 1].id}
+                >
                   <motion.h2
                     className="md:text-title text-Mtitle"
                     animate={animateFromRight}
